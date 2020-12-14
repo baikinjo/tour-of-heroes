471b90a4a4c611f965974d4770a3e0bb63a35b00
Injo Baik <baikinjo.28@gmail.com>
2020-12-14 10:39:37 -0800

    02-mock heroes, display in list using *ngFor; onSelect with *ngIf

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 10, name: 'Iron Man' },
+  { id: 11, name: 'Captain America' },
+  { id: 12, name: 'Thor' },
+  { id: 13, name: 'Hulk' },
+  { id: 14, name: 'Black Widow' },
+  { id: 15, name: 'Hawkeye' },
+  { id: 16, name: 'Vision' },
+  { id: 17, name: 'Falcon' },
+  { id: 18, name: 'Scarlet Witch' },
+  { id: 19, name: 'Ant Man' },
+];
